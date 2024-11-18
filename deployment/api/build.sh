ef886db5d9c5391f858895417dc95cdf6537b735
Azamat Holmeer <azamat.holmeer@gmail.com>
2024-11-18 20:51:34 +0500

    feature: use docker build action

--- deployment/api/build.sh
+++ deployment/api/build.sh
@@ -8,10 +8,3 @@ source ./deployment/api/_vars.sh
 
 # Build dist
 npx nx run api:build
-
-# Build & push 'api' docker image
-docker build \
-  --platform linux/amd64 \
-  --tag ${API_DOCKER_IMAGE} \
-  --file ./deployment/api/Dockerfile .
-docker push ${API_DOCKER_IMAGE}
