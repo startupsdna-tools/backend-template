fd8d68187034f0e48a09a321c141ea9362ad060d
Azamat Holmeer <azamat.holmeer@gmail.com>
2024-12-09 14:36:01 +0500

    removed: nest/webpack, added: esbuild

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,20 +2,13 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
+import * as http from 'node:http';
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+});
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
-}
-
-bootstrap();
+server.listen(3000, () => {
+  console.log('Listening at http://localhost:3000');
+});
