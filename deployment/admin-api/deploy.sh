162ed6d80962fcfbbbac5ef2c1ce872f1b0621b6
Azamat Holmeer <azamat.holmeer@gmail.com>
2024-12-16 12:42:20 +0500

    update: admin-api deployment vars

--- deployment/admin-api/deploy.sh
+++ deployment/admin-api/deploy.sh
@@ -5,7 +5,11 @@ set -eu
 
 source ./deployment/admin-api/_vars.sh
 
-## Deploy api service
+## define the service spec file location
 SERVICE_SPEC="deployment/admin-api/service.yaml"
-yq -i ".spec.template.spec.containers[0].image = \"${ADMIN_API_DOCKER_IMAGE}\"" ${SERVICE_SPEC}
+
+## replace environment variables in the service spec
+yq -i '(.. | select(tag == "!!str")) |= envsubst' ${SERVICE_SPEC}
+
+## deploy service to GCP
 gcloud run services replace ${SERVICE_SPEC} --region=${GCP_REGION} --project=${GCP_PROJECT}
