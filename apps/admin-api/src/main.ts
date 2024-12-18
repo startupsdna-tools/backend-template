25f2479995316909445264c5a3add25c6a5b20c0
Azamat Holmeer <azamat.holmeer@gmail.com>
2024-12-18 15:41:06 +0500

    add: logger level option

--- apps/admin-api/src/main.ts
+++ apps/admin-api/src/main.ts
@@ -1,4 +1,4 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyLoggerOptions } from 'fastify';
 import { createGcpLoggingPinoConfig } from '@google-cloud/pino-logging-gcp-config';
 import { app } from './app/app';
 
@@ -8,17 +8,27 @@ const port = Number(process.env.ADMIN_API_PORT ?? process.env.PORT ?? 3000);
 const prefix = process.env.ADMIN_API_PREFIX ?? '/api';
 
 // Configure logging
+const loggerOptions: FastifyLoggerOptions = {
+  level: process.env.LOGGER_LEVEL || 'info',
+};
+
 const logger = isGcp
-  ? createGcpLoggingPinoConfig({
-      serviceContext: {
-        service: 'admin-api',
+  ? createGcpLoggingPinoConfig(
+      {
+        serviceContext: {
+          service: 'admin-api',
+        },
       },
-    })
-  : true;
+      loggerOptions,
+    )
+  : loggerOptions;
 
 // Instantiate Fastify with some config
 const server = Fastify({
   logger: logger,
+
+  // Disable request logging when running on GCP,
+  // since all requests are logged by Cloud Run automatically.
   disableRequestLogging: isGcp,
 });
 
