cd20f73eadd03636de689d50a304edf89fcb073f
Azamat Holmeer <azamat.holmeer@gmail.com>
2024-12-19 10:35:51 +0500

    add: docker build cache

--- deployment/api/build.sh
+++ deployment/api/build.sh
@@ -9,6 +9,8 @@ source ./deployment/api/_vars.sh
 # Build & push 'api' docker image
 docker build \
   --platform linux/amd64 \
+  --cache-to type=local,dest=.docker/cache \
+  --cache-from type=local,dest=.docker/cache \
   --tag ${API_DOCKER_IMAGE} \
+  --push \
   --file ./deployment/api/Dockerfile .
-docker push ${API_DOCKER_IMAGE}
